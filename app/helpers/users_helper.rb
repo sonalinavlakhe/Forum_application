8f3c162afd9d9cd4a487ac0fe75a0b5d64e2dc2c
Sonali Bhavsar <sonalibhavsar977@gmail.com>
2020-08-27 12:53:43 +0530

    Add profile image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def profile_picture(user, width = 100)
+    image_path = "placeholder-profile.jpg"
+    image_tag(image_path, width: width, class: "img-circle m-r-10 rounded-circle")
+  end
 end
